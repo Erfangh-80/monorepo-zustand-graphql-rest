97a2f125f6d77b5be4732ce0cea3cceb6192ebd2
Erfangh-80 <erfangh4871gholami@gmail.com>
2024-01-18 12:07:54 +0330

    use page and fetch data ssr

--- apps/fr/prisma-graphql/pages/_app.tsx
+++ apps/fr/prisma-graphql/pages/_app.tsx
@@ -1,17 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
-
+import { Provider, useCreateStore } from 'states';
 function CustomApp({ Component, pageProps }: AppProps) {
+  const createStore: any = useCreateStore(pageProps.initialZustandState);
   return (
-    <>
+    <Provider createStore={createStore}>
       <Head>
-        <title>Welcome to prisma-graphql!</title>
+        <title>Welcome to prisma-postgres!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </>
+    </Provider>
   );
 }
 
